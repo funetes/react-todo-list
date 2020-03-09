1f81ef4d5e6d8778e1cdeb5a1f2546f48c9b2fa1
kimhwan <kimlove2324@gmail.com>
2020-03-09 15:46:24 +0900

    css부분 일부 완료

--- src/component/App.js
+++ src/component/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import GroupLists from './GroupLists'
+import Todos from './Todos'
+import AddTodos from './AddTodos'
 import '../css/App.css';
 
 function App() {
   return (
     <div className="App">
+      <div className="App__columns">
+        <GroupLists />
+      </div>
+      <div className="App__columns">
+        <div>
+          <span>ToDoList</span>
+          <AddTodos />
+        </div>
+        <Todos />
+      </div>
     </div>
   );
 }
